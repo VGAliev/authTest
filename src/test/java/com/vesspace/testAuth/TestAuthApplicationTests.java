f10a830dce1a00116f92f1f018977ec5a064e216
VGAliyev <v.aliev84@yandex.ru>
2024-03-29 09:41:50 +0400

    Initial commit

---
+++ src/test/java/com/vesspace/testAuth/TestAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vesspace.testAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
